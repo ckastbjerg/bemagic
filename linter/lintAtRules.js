825303ded90ef8779d2af073dd7453cc13495859
Christian Kastbjerg <ck@issuu.com>
2019-09-18 12:23:45 +0200

    more helpful warnings

--- linter/lintAtRules.js
+++ linter/lintAtRules.js
@@ -2,11 +2,23 @@ module.exports = obj => {
     const errors = [];
 
     if (!obj.atRules || !obj.atRules.description) {
-        errors.push(new Error(`${obj.name} has no \`description\`. All classes must have a description when using bemagic's \`strict\` mode.`));
+        errors.push(new Error(`
+            ${obj.name} has no \`description\`. 
+            All classes must have a description when using bemagic's \`strict\` mode.
+
+            Classes:
+            ${JSON.stringify(obj.classes)}
+        `));
     }
 
     if (obj.elements && !obj.atRules.intro) {
-        errors.push(new Error(`${obj.name} has no \`intro\`. All component base classes must have an intro when using bemagic's \`strict\` mode.`));
+        errors.push(new Error(`
+            ${obj.name} has no \`intro\`. 
+            All component base classes must have an intro when using bemagic's \`strict\` mode.
+
+            Classes:
+            ${JSON.stringify(obj.classes)}
+        `));
     }
 
     return errors.length > 0 ? errors : null;
